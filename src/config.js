f84d0c430d25c70baaa4d78794daa60366a1c84a
ronghaoZHI <18811012720@163.com>
2021-03-11 16:23:45 +0800

    update js version

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const IS_DEBUG = true;
 console.log('IS_DEBUG', IS_DEBUG);
 
-const version = '1.1.3';
+const version = '1.1.4';
 const appkey = IS_DEBUG ?
   "7e3aa05dd28eca88aebac6697635ccb8" :
   "15072b795aa21f522561adfca1c1d78b";
